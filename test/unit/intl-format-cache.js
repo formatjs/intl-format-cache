60dbdb5c3b59bc3d5d61a262529f89ea43b37172
Ryuichi Okumura <okuryu@okuryu.com>
2018-10-31 21:51:09 +0900

    Tweak latest expect API

--- test/unit/intl-format-cache.js
+++ test/unit/intl-format-cache.js
@@ -3,8 +3,9 @@
 describe('intl-format-cache', function () {
     it('has a function as the default export', function () {
         expect(memoizeFormatConstructor)
-            .toExist()
-            .toBeA('function');
+            .toBeTruthy();
+        expect(typeof memoizeFormatConstructor)
+            .toBe('function');
     });
 
     describe('Intl built-ins', function () {
@@ -16,10 +17,10 @@ describe('intl-format-cache', function () {
 
                 expect(df.resolvedOptions().locale).toBe('en');
                 // Lack of tz support, so just check that it returns a string.
-                expect(df.format(0)).toBeA('string');
+                expect(typeof df.format(0)).toBe('string');
 
                 expect(getDateTimeFromat('en')).toBe(df);
-                expect(getDateTimeFromat('en', {year: 'numeric'})).toNotBe(df);
+                expect(getDateTimeFromat('en', {year: 'numeric'})).not.toBe(df);
             });
         });
 
@@ -33,7 +34,7 @@ describe('intl-format-cache', function () {
                 expect(nf.format(1000)).toBe('1,000');
 
                 expect(getNumberFormat('en')).toBe(nf);
-                expect(getNumberFormat('en', {style: 'percent'})).toNotBe(nf);
+                expect(getNumberFormat('en', {style: 'percent'})).not.toBe(nf);
             });
         });
     });
@@ -48,7 +49,7 @@ describe('intl-format-cache', function () {
             expect(mf.format()).toBe('foo');
 
             expect(getMessageFormat('foo', 'en')).toBe(mf);
-            expect(getMessageFormat('bar', 'en')).toNotBe(mf);
+            expect(getMessageFormat('bar', 'en')).not.toBe(mf);
         });
     });
 
@@ -62,7 +63,7 @@ describe('intl-format-cache', function () {
             expect(rf.format(0, {now: 1000})).toBe('1 second ago');
 
             expect(getRelativeFormat('en')).toBe(rf);
-            expect(getRelativeFormat('en', {units: 'hour'})).toNotBe(rf);
+            expect(getRelativeFormat('en', {units: 'hour'})).not.toBe(rf);
         });
     });
 });
